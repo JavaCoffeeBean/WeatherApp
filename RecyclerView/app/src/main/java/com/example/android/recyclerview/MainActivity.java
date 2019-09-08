ece93b0ed99c184eff8f92ccfee1c322d2e7d6ee
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2019-09-08 15:00:59 -0500

    added things

--- RecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.android.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started.");
+    }
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps");
+
+
     }
 }
